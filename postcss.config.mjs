21022d940ba395828e5ba74b137cca19216900a3
Maizer <vasilbazhiv12@gmail.com>
2024-04-21 18:41:45 +0300

    add fix postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,11 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    // other PostCSS plugins if you have any
-  },
+// plugins: {
+//   // tailwindcss: {},
+//   // autoprefixer: {},
+//   // other PostCSS plugins if you have any
+// },
+
+const plugins = ['tailwindcss', 'autoprefixer'];
+
+export default {
+  plugins,
 };
