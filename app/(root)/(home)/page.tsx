feaf7fe0691b854377edf2bbc6da8eeb92d8330a
Maizer <vasilbazhiv12@gmail.com>
2024-05-02 13:43:14 +0300

    create first form

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <div className="mb-8 flex flex-col-reverse items-center justify-between gap-4 sm:flex-row">
         <h1 className="h1-bold font-inter text-dark-100 dark:text-light-900">All Questions</h1>
-        <Link href="/" className="block w-full max-w-full sm:w-auto">
+        <Link href="/ask-question" className="block w-full max-w-full sm:w-auto">
           <Button className="primary-gradient base-bold w-full text-light-900">Ask a Question</Button>
         </Link>
       </div>
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,13 @@
+import AskForm from '@/components/Forms/AskForm';
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+      <h1 className="h1-bold mb-9 font-inter">Ask a public question</h1>
+      <AskForm />
+    </>
+  );
+};
+
+export default page;
