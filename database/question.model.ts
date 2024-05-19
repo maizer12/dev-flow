634de9c48a0176cb1294b2d61894ce5d9db22767
Maizer <vasilbazhiv12@gmail.com>
2024-05-19 19:37:50 +0300

    create first models

---
+++ database/question.model.ts
@@ -0,0 +1,38 @@
+import { Schema, models, model, Document } from 'mongoose';
+
+export interface IQuestion extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[];
+  downvotes: Schema.Types.ObjectId[];
+  author: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
+}
+
+const QuestionSchema = new Schema<IQuestion>({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  tags: [{ type: Schema.Types.ObjectId, ref: 'Tag', default: [] }],
+  views: {
+    type: Number,
+    default: 0,
+  },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: 'User', default: [] }],
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
+  answers: [{ type: Schema.Types.ObjectId, ref: 'Answer', default: [] }],
+  createdAt: { type: Date },
+});
+
+const Question = models.Question || model('Question', QuestionSchema);
+
+export default Question;
